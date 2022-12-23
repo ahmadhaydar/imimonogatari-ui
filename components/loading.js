99ed084d16291163007a61812230cc23842b1ba9
Haydar <alkaf.haydar@gmail.com>
2022-12-23 12:41:46 +0700

    monogatari loading

--- components/loading.js
+++ components/loading.js
@@ -1,4 +1,4 @@
-import { Flex, Heading, Stack, Spinner } from "@chakra-ui/react";
+import { Flex, Heading, Stack, Spinner, HStack, ChakraProvider } from "@chakra-ui/react";
 
 export const LoadingBox = () => {
   // a box that shows a loading animation
@@ -11,15 +11,57 @@ export const LoadingBox = () => {
       top="0"
       left="0"
       zIndex="100"
-      bg="rgba(0,0,0,0.5)"
+      bg="yellow.300"
       align="center"
       justify="center"
     >
       <Stack align="center" spacing={3}>
-        <Spinner size="xl" color="white" thickness="5px"/>
-        <Heading size="lg" color="white">
+        <HStack>
+          <Stack align="center">
+            <Heading size="xl" color="white">
+              読
+            </Heading>
+            <Heading size="sm" color="white">
+              Yo
+            </Heading>
+          </Stack>
+          <Stack align="center">
+            <Heading size="xl" color="white">
+            み
+            </Heading>
+            <Heading size="sm" color="white">
+              Mi
+            </Heading>
+          </Stack>
+          <Stack align="center">
+            <Heading size="xl" color="white">
+            込
+            </Heading>
+            <Heading size="sm" color="white">
+              Ko
+            </Heading>
+          </Stack>
+          <Stack align="center">
+            <Heading size="xl" color="white">
+            み
+            </Heading>
+            <Heading size="sm" color="white">
+              Mi
+            </Heading>
+          </Stack>
+          <Stack align="center">
+            <Heading size="xl" color="white">
+            中
+            </Heading>
+            <Heading size="sm" color="white">
+              Chuu
+            </Heading>
+          </Stack>
+        </HStack>
+        <Heading size="md" color="white">
           Loading...
         </Heading>
+        <Spinner size="xl" color="white" thickness="5px" />
       </Stack>
     </Flex>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <title>意味物語 - Imimonogatari</title>
+    </Head>
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
+    </>
   )
 }
 
